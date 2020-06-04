17585879adaf26734231c9c573cde9fb0e7e2cf7
Jan Krivanek <jan.krivanek@solarwinds.com>
2020-06-04 18:29:24 +0200

    PA-5074: InMemoryCaching - securing connection

--- InMemoryCaching/InMemoryCacheUser/Program.cs
+++ InMemoryCaching/InMemoryCacheUser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Security.Authentication;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Caching.Redis;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Options;
 using SolarWinds.InMemoryCachingUtils;
+using StackExchange.Redis;
 
 namespace InMemoryCacheUser
 {
@@ -34,8 +36,23 @@ namespace InMemoryCacheUser
             serviceCollection.AddOptions();
             serviceCollection.Configure((Action<RedisCacheOptions>) (options =>
             {
-                options.Configuration = "sharedCache:6379";
-                options.InstanceName = "redisInstance";
+                //options.Configuration = "sharedCache:6379";
+                //options.InstanceName = "redisInstance";
+                options.ConfigurationOptions = new ConfigurationOptions()
+                {
+                    //TODO: those should go through IConfiguration
+                    EndPoints = { "sharedCache:6379" },
+                    Password = Environment.GetEnvironmentVariable("REDIS_PASSWORD"),
+                    //For this we'd need to enable the ssl in redis.conf http://download.redis.io/redis-stable/redis.conf
+                    // then we can either build the new image with that: https://hub.docker.com/_/redis/
+                    // or inject it via shared volumes: http://kb.objectrocket.com/redis/run-redis-with-docker-compose-1055#create+a+docker-compose+file+for+redis
+                    //HOWEVER - we won't very likely use the redis container, but aws ElastiCache - the encryption (in-transit and at-rest) should
+                    // be configured directly there (and then either terminated in side car, or in the code - that needs to be investigated):
+                    //   https://aws.amazon.com/about-aws/whats-new/2017/10/amazon-elasticache-for-redis-now-supports-in-transit-and-at-rest-encryption-to-help-protect-sensitive-information/
+                    //Ssl = true,
+                    //SslProtocols = SslProtocols.Tls
+
+                };
             }));
             serviceCollection.AddScoped<RedisCache>();
 
@@ -48,6 +65,7 @@ namespace InMemoryCacheUser
             serviceCollection.AddLogging(opt =>
             {
                 opt.AddConsole(copt => { copt.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fffffff"; });
+                opt.SetMinimumLevel(LogLevel.Trace);
             });
 
 
